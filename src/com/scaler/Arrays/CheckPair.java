078241e3d73da589838b48815d8e39c437d4de9f
Mohamed M <safiizz24@gmail.com>
2022-08-27 07:43:15 +0530

    checking pairs with optimization, fewer iterations

--- src/com/scaler/Arrays/CheckPair.java
+++ src/com/scaler/Arrays/CheckPair.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class CheckPair {
     //No of Iterations are higher
-    public int solve(ArrayList<Integer> A, int B) {
+    public int solution1(ArrayList<Integer> A, int B) {
         int N = A.size();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -21,6 +21,18 @@ public class CheckPair {
         return 0;
     }
 
+    public int solution2(ArrayList<Integer> A, int B) {
+        int N = A.size();
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (A.get(i) + A.get(j) == B) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         CheckPair goodPair = new CheckPair();
         ArrayList<Integer> aList = new ArrayList<>();
@@ -30,7 +42,12 @@ public class CheckPair {
         aList.add(8);
         aList.add(5);
         int B = 5;
-        int result = goodPair.solve(aList, B);
-        System.out.println(result);
+        int result1 = goodPair.solution1(aList, B);
+        System.out.println(result1);
+        int C = 8;
+        int result2 = goodPair.solution1(aList, B);
+        System.out.println(result2);
+        System.out.println(goodPair.solution1(aList, 1));
+        System.out.println(goodPair.solution2(aList, 1));
     }
 }
