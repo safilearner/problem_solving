3cc1001f4b8824585b70be84c3c95d3ed979a97a
Mohamed M <safiizz24@gmail.com>
2022-08-29 00:06:06 +0530

    Sub Array: Print Sum Of All Sub Arrays, with carry forward technique and brute force technique

--- src/com/scaler/Arrays/SumOfAllSubArrays.java
+++ src/com/scaler/Arrays/SumOfAllSubArrays.java
@@ -2,22 +2,40 @@ package com.scaler.Arrays;
 
 public class SumOfAllSubArrays {
     public static void main(String[] args) {
-        int[] inputArray = {5, 1, 2, 3, 6, 7, -1, -2, 9};
+        //int[] inputArray = {5, 1, 2, 3, 6, 7, -1, -2, 9};
+        int[] inputArray = {5, 1, 2, 3};
         SumOfAllSubArrays sumOfSub = new SumOfAllSubArrays();
-        sumOfSub.sumOfSubArrays(inputArray);
+        sumOfSub.sumOfSubArraysII(inputArray);
+        System.out.println("");
+        sumOfSub.sumOfSubArraysI(inputArray);
+    }
+
+    //O(N^3) - Time Complexity
+    public void sumOfSubArraysI(int[] A) {
+        int N = A.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j < N; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += A[k];
+                }
+                System.out.print(sum + " ");
+            }
+        }
     }
 
     //Print All Sub Arrays Sum
     //Time Complexity is O(N*N)
-    public void sumOfSubArrays(int[] A) {
+    //Carry Forward Technique
+    public void sumOfSubArraysII(int[] A) {
         int N = A.length;
         //Getting all sub arrays
         //j all starts at i for sub arrays
-        int sum = 0;
         for (int i = 0; i < N; i++) {
+            int sum = 0;
             for (int j = i; j < N; j++) {
                 sum = sum + A[j];
-                System.out.println(sum);
+                System.out.print(sum + " ");
             }
         }
     }
