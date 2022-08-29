9140104bd5199021e28e6bd259f863a3fed95c67
Mohamed M <safiizz24@gmail.com>
2022-08-29 10:03:20 +0530

    Prime Number Check: Simple Solution

--- src/com/samples/checkPrime.java
+++ src/com/samples/checkPrime.java
@@ -1,20 +1,29 @@
 package com.samples;
 
+import java.util.Scanner;
+
 public class checkPrime {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int prime = sc.nextInt();
+        System.out.println("Whether the given number is prime? " + isPrime(prime));
+        sc.close();
     }
 
-    public boolean isPrime(int N) {
-        if (N == 1) {
-            return false;
-        } else if (N == 2) {
+    //Check if the number is prime
+    //A number is prime if it has exactly two factors
+    //1 and itself are prime
+    public static boolean isPrime(int N) {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            //Modules operator is to find the remainder
+            if (N % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
             return true;
         }
-        for (int i = 2; i <= 2; i++) {
-
-        }
-
         return false;
     }
 }
